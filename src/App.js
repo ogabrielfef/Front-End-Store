18d14d2de2facd1200cfddc6932ae5e46d639aab
Otávio Azevedo <98931133+otavioadias@users.noreply.github.com>
2022-05-24 21:57:12 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Link, Switch, Router } from 'react-router-dom';
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
 import './App.css';
 import Cart from './Component/Cart';
 import Home from './Component/Home';
