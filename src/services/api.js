ae5748dbe65e4a93f7043865749e9c8f1ef264bd
Dihrey Araujo <99283526+dihreyaraujo@users.noreply.github.com>
2022-05-24 15:30:50 -0300

    lint update

--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,12 @@
 export async function getCategories() {
-  const url = "https://api.mercadolibre.com/sites/MLB/categories";
+  const url = 'https://api.mercadolibre.com/sites/MLB/categories';
   const response = await fetch(url);
   const categories = await response.json();
   return categories;
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
-  const url = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}_ID&q=${query}`
+  const url = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}_ID&q=${query}`;
   const response = await fetch(url);
   const results = await response.json();
   return results;
